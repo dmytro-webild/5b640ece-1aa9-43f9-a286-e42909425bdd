32985a9fd3deb56f343674cd18f0e359b5a01939
bender <kudin.dima@gmail.com>
2026-03-05 15:07:05 +0000

    Update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -68,7 +68,11 @@ const HomePage = () => {
       headingFontWeight="normal"
     >
       <div id="nav" data-section="nav">
-        <NavbarStyleApple brandName="Aajel" navItems={navItems} />
+        <NavbarStyleApple 
+          brandName="Aajel" 
+          navItems={navItems}
+          logoImageSrc="https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3AWxsIwZ7faFTOMvnFS8qknLwYR/uploaded-1772723192460-1v0n4pt8.png"
+        />
       </div>
 
       <div id="hero" data-section="hero">
